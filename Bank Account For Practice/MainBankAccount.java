fc3e441d5e79537197a41a5b6a6fb8e6c81e534d
Heindrich Macalinao <heindrich.macalinao@gmail.com>
2026-02-27 23:19:07 +0800

    Add documentation comments to MainBankAccount class
    Added documentation comments to describe the program's purpose and functionality.

--- Bank Account For Practice/MainBankAccount.java
+++ Bank Account For Practice/MainBankAccount.java
@@ -1,3 +1,10 @@
+/**
+ * Practice: Bank Account Management System
+ * Date: February 19
+ * Description: This program demonstrates Object-Oriented Programming (OOP) 
+ * by instantiating multiple BankAccount objects using a parameterized 
+ * constructor and displaying their individual data.
+ */
 public class MainBankAccount {
     public static void main(String[] args) {
 
@@ -11,4 +18,19 @@ public class MainBankAccount {
         acc1.displayAccount();
         acc2.displayAccount();
     }
+}
+public class MainBankAccount {
+    public static void main(String[] args) {
+
+        // Creating first object with different values
+        BankAccount acc1 = new BankAccount("Maria", 5000.50);
+
+        // Creating second object with different values
+        BankAccount acc2 = new BankAccount("Juan", 12000.75);
+
+        // Calling method for each object
+        acc1.displayAccount();
+        acc2.displayAccount();
+    }
+
 }
