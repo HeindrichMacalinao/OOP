6a23f3a18c0f9d02e88be115ebb20f8c2f8fa782
Heindrich Macalinao <heindrich.macalinao@gmail.com>
2026-02-28 00:00:44 +0800

    Add files via upload

---
+++ Classes & Objects Review/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Creating objects (instances) of Student class
+        Student student1 = new Student("Alice Johnson", 12345, "Computer Science");
+        Student student2 = new Student("Bob Smith", 12346, "Engineering");
+
+        // Setting GPA values
+        student1.setGpa(3.8);
+        student2.setGpa(3.5);
+
+        // Displaying student information
+        System.out.println("=== Student 1 Information ===");
+        student1.displayInfo();
+        System.out.println("=== Student 2 Information ===");
+        student2.displayInfo();
+    }
+}
